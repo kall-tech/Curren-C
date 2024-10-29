aead27df2769b6a15c587df415462d5fa5cb75f2
kall-tech <157445714+kall-tech@users.noreply.github.com>
2024-10-29 16:56:40 +1100

    Initial Commit

---
+++ app/src/main/java/com/kalltech/currenc_converter/utils/constants.kt
@@ -0,0 +1,8 @@
+package com.kalltech.currenc_converter.utils
+
+object Constants {
+    const val API_BASE_URL = "https://v6.exchangerate-api.com/"
+    const val API_KEY = "API-KEY"
+    const val UPDATE_FREQUENCY_DAYS = 7L // Update frequency in days
+    const val DATABASE_NAME = "currency_db"
+}
