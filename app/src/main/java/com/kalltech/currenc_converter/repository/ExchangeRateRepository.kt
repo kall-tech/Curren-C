c0a4d4201864b375972b0669613ac09e6e4ee7b9
kall-tech <157445714+kall-tech@users.noreply.github.com>
2024-10-31 23:08:10 +1100

    Cleanup fetch rates function

--- app/src/main/java/com/kalltech/currenc_converter/repository/ExchangeRateRepository.kt
+++ app/src/main/java/com/kalltech/currenc_converter/repository/ExchangeRateRepository.kt
@@ -16,67 +16,54 @@ class ExchangeRateRepository(
     private val apiService: ExchangeRateApiService,
     private val exchangeRateDao: ExchangeRateDao,
     private val lastUpdateDao: LastUpdateDao
+
 ) {
+    private suspend fun fetchAndSaveExchangeRates(baseCurrency: String): Result<Unit> {
+    return try {
+        val response = apiService.getLatestRates(Constants.API_KEY, baseCurrency)
+        if (response.isSuccessful) {
+            Log.d("ExchangeRateRepository", "API call successful")
+            val body = response.body()
+            if (body != null && body.result == "success") {
+                val rates = body.conversionRates?.map {
+                    ExchangeRateEntity(it.key, it.value)
+                } ?: emptyList()
+                exchangeRateDao.insertRates(rates)
+                lastUpdateDao.insertLastUpdate(LastUpdateEntity(timestamp = System.currentTimeMillis()))
+                Result.success(Unit)
+            } else {
+                Log.e("ExchangeRateRepository", "API call failed: ${response.errorBody()?.string()}")
+                Result.failure(Exception("API Error"))
+            }
+        } else {
+            Result.failure(Exception("Network Error"))
+        }
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
+}
     suspend fun updateExchangeRates(baseCurrency: String): Result<Unit> {
         return withContext(Dispatchers.IO) {
-            try {
                 val lastUpdate = lastUpdateDao.getLastUpdate()
                 val currentTime = System.currentTimeMillis()
                 val updateFrequencyMillis = TimeUnit.DAYS.toMillis(Constants.UPDATE_FREQUENCY_DAYS)
 
                 val needsUpdate = lastUpdate == null || (currentTime - lastUpdate.timestamp) > updateFrequencyMillis
+
                 //val needsUpdate = true //for debugging
                 if (needsUpdate) {
-                    val response = apiService.getLatestRates(Constants.API_KEY, baseCurrency)
-                    if (response.isSuccessful) {
-                        Log.d("ExchangeRateRepository", "API call successful")
-                        val body = response.body()
-                        if (body != null && body.result == "success") {
-                            val rates = body.conversionRates?.map {
-                                ExchangeRateEntity(it.key, it.value)
-                            } ?: emptyList()
-                            exchangeRateDao.insertRates(rates)
-                            lastUpdateDao.insertLastUpdate(LastUpdateEntity(timestamp = currentTime))
-                            Result.success(Unit)
-                        } else {
-                            Log.e("ExchangeRateRepository", "API call failed: ${response.errorBody()?.string()}")
-                            Result.failure(Exception("API Error"))
-                        }
-                    } else {
-                        Result.failure(Exception("Network Error"))
-                    }
+                    Log.d("ExchangeRateRepository", "Exchange Rates older than a week! Updating exchange rates...")
+                    fetchAndSaveExchangeRates(baseCurrency)
                 } else {
                     Result.success(Unit)
                 }
-            } catch (e: Exception) {
-                Log.e("ExchangeRateRepository", "API call failed: ${e.message}")
-                Result.failure(e)
-            }
+
         }
     }
 
-    suspend fun forceUpdateExchangeRates(): Result<Unit> {
+    suspend fun forceUpdateExchangeRates(baseCurrency: String): Result<Unit> {
         return withContext(Dispatchers.IO) {
-            try {
-                val response = apiService.getLatestRates(Constants.API_KEY, "USD")
-                if (response.isSuccessful) {
-                    val body = response.body()
-                    if (body != null && body.result == "success") {
-                        val rates = body.conversionRates?.map {
-                            ExchangeRateEntity(it.key, it.value)
-                        } ?: emptyList()
-                        exchangeRateDao.insertRates(rates)
-                        lastUpdateDao.insertLastUpdate(LastUpdateEntity(timestamp = System.currentTimeMillis()))
-                        Result.success(Unit)
-                    } else {
-                        Result.failure(Exception("API Error"))
-                    }
-                } else {
-                    Result.failure(Exception("Network Error"))
-                }
-            } catch (e: Exception) {
-                Result.failure(e)
-            }
+            fetchAndSaveExchangeRates(baseCurrency) // Call fetchAndSaveRates here with "USD"
         }
     }
 
